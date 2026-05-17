0aa14d64c7a6115dfa8b803dd59caae8e3378cec
daviralbajpai-cloud <daviralbajpai@gmail.com>
2026-05-17 11:56:43 +0000

    object

---
+++ object/script.js
@@ -0,0 +1,11 @@
+const user = {
+
+    username: "Aviral Bajpai",
+    address: {
+        city: "Delhi",
+        state: "Delhi",
+        country: "India"
+    }       
+}
+user.age = 21
+user["is-online"]= true
---
+++ ternary_operator/script.js
@@ -0,0 +1,6 @@
+const userage = 19
+const userage2 = 17
+const message = userage >= 18 ? "Welcome!" : "Access Denied";
+const message2 = userage2 >= 18 ? "Welcome!" : "Access Denied";
+console.log(message)
+console.log(message2)
