b47dfa5c1aa7655aeb5f93952c80614950f99fbe
daviralbajpai-cloud <daviralbajpai@gmail.com>
2026-05-28 10:16:34 +0000

    update

---
+++ DOM/script.js
@@ -0,0 +1,7 @@
+// console.log(window)
+// console.lod(window.document)
+// console.log(document)
+// console.dir(document)
+
+//wikipedia
+// document.getElementById('firstHeading').innerHTML = "Aviral"
